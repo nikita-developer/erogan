02a3299f953f5a81059921c448637dc4adaab9d3
Nikita <cycyrbita@mail.ru>
2020-12-07 12:16:11 +0300

    блок commits готов

--- src/js/index.js
+++ src/js/index.js
@@ -48,22 +48,22 @@ $(document).ready(function () {
         }, 1000);
     }
 
-    // $('.js-commits__box').owlCarousel({
-    //     loop: true,
-    //     margin: 40,
-    //     autoHeight: true,
-    //     responsive: {
-    //         320: {
-    //             items: 1
-    //         },
+    $('.js-commits__box').owlCarousel({
+        loop: true,
+        margin: 40,
+        autoHeight: true,
+        responsive : {
+            320 : {
+                items: 1
+            },
 
-    //         768: {
-    //             items: 2
-    //         },
+            768 : {
+                items: 2
+            },
 
-    //         1280: {
-    //             items: 3
-    //         }
-    //     }
-    // });
+            1280 : {
+                items: 3
+            }
+        }
+    });
 });
